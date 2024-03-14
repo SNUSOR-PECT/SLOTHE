bd2cda9125420518a691144820ed85a68071486b
Youyeon <yourain416@gmail.com>
2024-03-14 15:51:36 +0900

    Modify RemoveSpecials Pass. Add the subNormal case too.

--- include/RemoveSpecials.h
+++ include/RemoveSpecials.h
@@ -13,6 +13,7 @@
 #define LLVM_TUTOR_REMOVESPECIALS_H
 
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -29,8 +30,12 @@ struct RemoveSpecials : public llvm::PassInfoMixin<RemoveSpecials> {
     public:
         explicit RemoveSpecials(llvm::raw_ostream &OutS) : OS(OutS) {}
         
-        llvm::Value* getOperandFromCondition(llvm::Value* Cond);
-        bool isSpecialBranch(llvm::BasicBlock* BB);
+        uint32_t getConstantVal(llvm::Value* val);
+        llvm::Value* getOperandFromCondition(llvm::Value* Cond, size_t pos);
+        bool isConditionHighwordofInput(llvm::Value* Cond);
+
+        bool isSpecialBranch(llvm::BasicBlock* BB, llvm::BranchInst *brInst);
+        bool isSubNormalBranch(llvm::BasicBlock* BB, llvm::BranchInst *brInst);
 
         llvm::PreservedAnalyses run(llvm::Function &F, llvm::FunctionAnalysisManager &);
 
--- src/RemoveSpecials.cpp
+++ src/RemoveSpecials.cpp
@@ -2,6 +2,7 @@
 
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -16,79 +17,125 @@ using namespace llvm;
 #define DEBUG_TYPE "remove-specials"
 
 //------------------------------------------------------------------------------
+// Util functions
+//------------------------------------------------------------------------------
+uint32_t RemoveSpecials::getConstantVal(Value* val) {
+  ConstantInt *CI = dyn_cast<ConstantInt>(val);
+  return CI->getSExtValue();
+}
+
+llvm::Value* RemoveSpecials::getOperandFromCondition(Value* Cond, size_t pos) {
+  ICmpInst *ICmp = dyn_cast<ICmpInst>(Cond);
+  return ICmp->getOperand(pos);
+}
+
+bool RemoveSpecials::isConditionHighwordofInput(Value* Cond) {
+  std::stack<llvm::Instruction*> insts;
+
+  Value* op = getOperandFromCondition(Cond, 0);
+  insts.push(dyn_cast<Instruction>(Cond)); // push branch condition
+
+  // trace the instructions consist of target operands
+  // this may not work -- need to be modified precisely
+  while(1) {
+      if (Instruction *I = dyn_cast<Instruction>(op)) {
+          insts.push(I);
+          op = I->getOperand(0);
+          continue;
+      }
+      break;
+  }
+
+  // detect {bitcast - lshr - trunc} sequence
+  std::vector<bool> isValidSequence(3, false);
+  while(!insts.empty()) {
+      Instruction* I = insts.top();
+      insts.pop(); // remove top instruction
+
+      if (isValidSequence[0] == false && I->getOpcode()==Instruction::BitCast) {
+          isValidSequence[0] = true;
+      }
+      if (isValidSequence[1] == false && I->getOpcode()==Instruction::LShr) {
+          isValidSequence[1] = true;
+      }
+      if (isValidSequence[2] == false && I->getOpcode()==Instruction::Trunc) {
+          isValidSequence[2] = true;
+      }
+  }
+
+  return isValidSequence[0] & isValidSequence[1] & isValidSequence[2];
+}
+
+//------------------------------------------------------------------------------
 // Crucial functions
 //------------------------------------------------------------------------------
 
-llvm::Value* RemoveSpecials::getOperandFromCondition(Value* Cond) {
-    ICmpInst *ICmp = dyn_cast<ICmpInst>(Cond);
-    return ICmp->getOperand(0);
+bool RemoveSpecials::isSpecialBranch(llvm::BasicBlock* BB, llvm::BranchInst *brInst) {
+  Value* Cond = brInst->getCondition();
+
+  // First, check if the intermediate is specials(INF/NaN)
+  uint32_t opI = getConstantVal(getOperandFromCondition(Cond, 1));
+  uint32_t opT = 0x7ff00000 - 1; // since the condition holds 'equal'
+  if (!(opI == opT))
+    return false;
+
+  // Then check if the other operand is high word of the input 
+  bool isCondHighWord = isConditionHighwordofInput(Cond);
+
+  return isCondHighWord;
 }
 
-bool RemoveSpecials::isSpecialBranch(llvm::BasicBlock* BB) {
-    std::stack<llvm::Instruction*> insts;
+bool RemoveSpecials::isSubNormalBranch(llvm::BasicBlock* BB, llvm::BranchInst *brInst) {
+  Value* Cond = brInst->getCondition();
 
-    auto term = BB->getTerminator();
-    BranchInst *brInst = dyn_cast<BranchInst>(term);
-    Value* cond = brInst->getCondition();
-    Value* op = getOperandFromCondition(cond);
+  // First, check if the intermediate is subNormal
+  uint32_t opI = getConstantVal(getOperandFromCondition(Cond, 1));
+  uint32_t opT = 0x3c800000; // subnormal value in ieee-754 representation
+  if (!(opI == opT))
+    return false;
 
-    insts.push(dyn_cast<Instruction>(cond)); // push branch condition
+  // Then check if the other operand is high word of the input 
+  bool isCondHighWord = isConditionHighwordofInput(Cond);
 
-    // trace the instructions consists of target operands
-    // this may not work -- need to be modified precisely
-    while(1) {
-        if (Instruction *I = dyn_cast<Instruction>(op)) {
-            insts.push(I);
-            op = I->getOperand(0);
-            continue;
-        }
-        break;
-    }
-
-    // detect {bitcast - lshr - trunc} sequence
-    std::vector<bool> isValidSequence(3, false);
-    while(!insts.empty()) {
-        Instruction* I = insts.top();
-        insts.pop(); // remove top instruction
-        // errs() << *I << "\n";
-
-        if (isValidSequence[0] == false && I->getOpcode()==Instruction::BitCast) {
-            isValidSequence[0] = true;
-        }
-        if (isValidSequence[1] == false && I->getOpcode()==Instruction::LShr) {
-            isValidSequence[1] = true;
-        }
-        if (isValidSequence[2] == false && I->getOpcode()==Instruction::Trunc) {
-            isValidSequence[2] = true;
-        }
-    }
-
-    return isValidSequence[0] & isValidSequence[1] & isValidSequence[2];
+  return isCondHighWord;
 }
 
 PreservedAnalyses RemoveSpecials::run(llvm::Function &Func,
                                       llvm::FunctionAnalysisManager &) {
-    // errs() << "\n- Start of Function [" << Func.getName() << "]\n";
+  bool specialFound = false;
+  bool subnormalFound = false;
+  
+  for (auto &BB : Func) {
+    auto term = BB.getTerminator(); // check BB which contains conditional branch
+    BranchInst *brInst = dyn_cast<BranchInst>(term);
 
-    for (auto &BB : Func) {
-      // We currently assume that only one BB holds INF/NaN branch among BBs.
-      // Thus, since `isMergeable=true` occurs only once throughout BBs, we break the loop
-      auto term = BB.getTerminator(); // check BB which contains conditional branch
-      if (BranchInst *brInst = dyn_cast<BranchInst>(term)) {
-        if (brInst->isConditional()) {
-            bool isMergeable = isSpecialBranch(&BB); // check the branch's semantic
-            if (isMergeable) {
-                // remove branch by setting the same successor on both True/False branch
-                // To remove other successors, apply simplify-cfg pass would be good choice :) 
-                errs() << "[*] BB    - isMergeable = True detected!\n\n";
-                brInst->setSuccessor(0, brInst->getSuccessor(1));
-                break;
-            }
+    if ( brInst && (brInst->isConditional())) {
+      if (!specialFound) {
+        bool isMergeable = isSpecialBranch(&BB, brInst); // check the branch's semantic
+          if (isMergeable) {
+              // remove branch by setting the same successor on both True/False branch
+              // To remove other successors, apply simplify-cfg pass would be good choice :) 
+              errs() << "[*] BB    - isMergeable = True detected!\n";
+              brInst->setSuccessor(0, brInst->getSuccessor(1));
+              specialFound = true;
+          }
+      }
+      if (!subnormalFound) {
+        bool isSubNormal = isSubNormalBranch(&BB, brInst); // check the branch's semantic
+        if (isSubNormal) {
+          errs() << "[*] BB    - isSubNormal = True detected!\n\n";
+          brInst->setSuccessor(0, brInst->getSuccessor(1));
+          subnormalFound = true;
         }
       }
     }
 
-    return llvm::PreservedAnalyses::all();
+    // We currently assume that only one BB holds INF/NaN or subNormal branch among BBs.
+    // Thus, since each case occurs only once throughout BBs, we break the loop
+    if (specialFound & subnormalFound) break;
+  }
+
+  return llvm::PreservedAnalyses::all();
 }
 
 //-----------------------------------------------------------------------------
