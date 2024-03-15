bfa2f2bc33ddb4f51beebc856ef78a89f61b3e15
Youyeon <yourain416@gmail.com>
2024-03-15 12:19:18 +0900

    Change run.sh ; to create remove-special applied .ll file

--- run.sh
+++ run.sh
@@ -10,7 +10,11 @@ if [ $1 -eq 0 ];then
 fi
 
 if [ $1 -eq 1 ];then
-    /usr/local/bin/opt -load-pass-plugin ./build/lib/libRemoveSpecials.so -passes=remove-specials,simplifycfg,dot-cfg -disable-output _tanh.ll
+    /usr/local/bin/opt -load-pass-plugin ./build/lib/libRemoveSpecials.so -passes=remove-specials,simplifycfg _tanh.ll -o _tanh_RemoveSpecials.bc
+    /usr/local/bin/llvm-dis _tanh_RemoveSpecials.bc
+    rm -rf _tanh_RemoveSpecials.bc
+    
+    /usr/local/bin/opt -load-pass-plugin ./build/lib/libRemoveSpecials.so -passes=dot-cfg -disable-output _tanh_RemoveSpecials.ll
     dot -Tpng -o ./assets/_tanh_RemoveSpecials.png ./._tanh.dot
     echo -e "\n[*] A new CFG is drawn at ./assets/_tanh_RemoveSpecials.png!"
 fi
