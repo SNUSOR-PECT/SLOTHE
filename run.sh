43d343e7df2b7b2b838ba7504b87ef9b50b991f6
Youyeon <yourain416@gmail.com>
2024-07-29 21:38:35 +0900

    clean files

--- run.sh
+++ run.sh
@@ -6,19 +6,25 @@ if [ $# -le 0 ];then
 fi
 
 if [ $1 -eq 0 ];then
-    echo -e "\n[*] Apply BasicTrace pass\n"
+    echo -e "\n[*] Apply BasicTrace pass on tanh\n"
+    /usr/local/bin/clang -c -emit-llvm ./math/_tanh.c -o _tanh.bc
+    /usr/local/bin/llvm-dis _tanh.bc
     /usr/local/bin/opt -load-pass-plugin ./build/lib/libBasicTrace.so -passes=basic-trace -disable-output _tanh.ll
+    rm -rf _tanh.bc _tanh.ll
 fi
 
 if [ $1 -eq 1 ];then
-    echo -e "\n[*] Apply RemoveSpecial and SimplifyCFG pass\n"
+    echo -e "\n[*] Apply RemoveSpecial and SimplifyCFG pass on tanh\n"
+    /usr/local/bin/clang -c -emit-llvm ./math/_tanh.c -o _tanh.bc
+    /usr/local/bin/llvm-dis _tanh.bc # output: _tanh.ll
     /usr/local/bin/opt -load-pass-plugin ./build/lib/libRemoveSpecials.so -passes=remove-specials,simplifycfg _tanh.ll -o _tanh_RemoveSpecials.bc
     /usr/local/bin/llvm-dis _tanh_RemoveSpecials.bc
-    rm -rf _tanh_RemoveSpecials.bc
     
     /usr/local/bin/opt -load-pass-plugin ./build/lib/libRemoveSpecials.so -passes=dot-cfg -disable-output _tanh_RemoveSpecials.ll
     dot -Tpng -o ./assets/_tanh_RemoveSpecials.png ./._tanh.dot
     echo -e "\n[*] A new CFG is drawn at ./assets/_tanh_RemoveSpecials.png!"
+    rm -rf _tanh*.bc _tanh*.ll
+    rm -rf ./.*.dot
 fi
 
 #if [ $1 -eq 2 ];then
