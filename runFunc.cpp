d28057b1c7b35b7e8312f52b052fc0b2fa9c7cf3
Youyeon <yourain416@gmail.com>
2025-05-14 15:46:08 +0900

    find valid input range for two path

--- runFunc.cpp
+++ runFunc.cpp
@@ -69,7 +69,7 @@ void initBB(std::string& path, std::vector<int>& BNums, std::map<int, std::vecto
     }
 }
 
-void getCondition(std::string& path, std::vector<std::pair<std::string, int>>& cond, std::map<int, std::vector<int>>& BB) {
+void getCondition(std::string& path, std::vector<std::pair<int, std::pair<std::string, double>>>& cond, std::map<int, std::vector<int>>& BB) {
     std::ifstream ifs(path);
     std::string line;
     int cmpIdx = 0;
@@ -78,13 +78,14 @@ void getCondition(std::string& path, std::vector<std::pair<std::string, int>>& c
         std::cerr << "Failed to open file.\n";
     } else {
         while (std::getline(ifs, line)) {
+            int curNode, predNode;
             // override
             if (line.find("; preds =") != std::string::npos) {
                 auto tokens = split(line, ';');
-                int curNode = eraseSpecial(tokens[0]);
+                curNode = eraseSpecial(tokens[0]);
 
                 tokens = split(line, '=');
-                int predNode = eraseSpecial(tokens[1]);
+                predNode = eraseSpecial(tokens[1]);
                 if (predNode == 1) continue; // skip
                 if (tokens.size() > 2) continue; // TODO: phi node
                 
@@ -97,13 +98,18 @@ void getCondition(std::string& path, std::vector<std::pair<std::string, int>>& c
                 auto tokens = split(line, ' ');
                 std::string tag = tokens[5]; // ult ugt ..
                 std::string _intval = tokens[8]; // intermediate
+
+                /* TODO
+                 * set variable uint32_t or int32_t by tag
+                */
+
                 uint32_t intval = static_cast<uint32_t>(std::stoul(_intval));
 
                 double dval;
                 uint64_t val = (uint64_t(intval) << 32); // Shift to high 32 bits
                 std::memcpy(&dval, &val, sizeof(dval));
-                std::cout << tag << ", dval = " << dval << "\n";
-                cond.push_back({tag, dval});
+                // std::cout << "curNode = " << curNode << " {" << tag << ", " << dval << "}\n";
+                cond.push_back({curNode, {tag, dval}});
             }
             // conditional branch
             if (line.find("br") != std::string::npos) {
@@ -122,6 +128,28 @@ void getCondition(std::string& path, std::vector<std::pair<std::string, int>>& c
     }
 }
 
+void findRange(std::map<int, std::vector<int>>& BB, std::vector<std::pair<int, std::pair<std::string, double>>>& cond, int node, std::pair<double, double>& rangeNode) {
+    double _min=1e-9, _max=1e+6;
+
+    for (int i=0; i<cond.size(); i++) {
+        if (BB[node][i] != -1) {
+            std::string tag = cond[i].second.first;
+            double ival = cond[i].second.second;
+
+            if (tag.find("g") != std::string::npos) {
+                if (BB[node][i] == 1) _min = ival > _min ? ival : _min;
+                else _max = ival < _max ? ival : _max;
+            } 
+            if (tag.find("l") != std::string::npos) {
+                if (BB[node][i] == 1) _max = ival < _max ? ival : _max;
+                else _min = ival > _min ? ival : _min;
+            }
+        }
+    }
+
+    rangeNode = {_min, _max};
+}
+
 int main(void) {
     std::string path = "./_tanh_UnreachablePath.ll";
 
@@ -131,7 +159,7 @@ int main(void) {
     initBB(path, BNums, BB);
 
     // 1. get condition for left/right path for a given .ll file
-    std::vector<std::pair<std::string, int>> cond;
+    std::vector<std::pair<int, std::pair<std::string, double>>> cond;
     getCondition(path, cond, BB);
 
     for (int i=0; i<BNums.size(); i++) {
@@ -142,7 +170,41 @@ int main(void) {
         std::cout << "\n";
     }
 
-    // 2. 
+    for (int i=0; i<cond.size(); i++) {
+        std::cout << "curNode = " << cond[i].first << " {" << cond[i].second.first << ", " << cond[i].second.second << "}\n";
+    }
+
+    // 2. detect two split path
+    int nodeA, nodeB;
+    // same T/Fs but different 0/1 at a certain branch
+    for (int i=1; i<BNums.size(); i++) {
+        nodeA = BNums[i-1];
+        nodeB = BNums[i];
+
+        bool flag = false;
+        for (int j=0; j<BB[BNums[i]].size(); j++) {
+            if (BB[nodeA][j] != BB[nodeB][j]) {
+                if (BB[nodeA][j] + BB[nodeB][j] == 1) {// 1/0 or 0/1
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag) break;
+    }
+
+    std::cout << "nodeA = " << nodeA << "\n";
+    std::cout << "nodeB = " << nodeB << "\n";
+
+    // 3. find valid input range for two path
+    std::pair<double, double> rangeNodeA, rangeNodeB; // min, max
+    findRange(BB, cond, nodeA, rangeNodeA);
+    findRange(BB, cond, nodeB, rangeNodeB);
+    
+    std::cout << "[*] nodeA\n";
+    std::cout << "_min = " << rangeNodeA.first << ", _max = " << rangeNodeA.second << "\n"; 
+    std::cout << "[*] nodeB\n";
+    std::cout << "_min = " << rangeNodeB.first << ", _max = " << rangeNodeB.second << "\n"; 
 
     // double x = 1.0;
     // double y = _tanh(x);
